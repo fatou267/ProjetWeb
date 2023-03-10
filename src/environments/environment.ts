ba648c7fa274b96d1ca3eb269a07dd62064fc994
lina zizi <zizilina003@gmail.com>
2023-03-10 11:21:52 +0100

    linas task

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'projet-api-64e59',
+    appId: '1:845732716561:web:f091a6a0a539afd750d1b0',
+    storageBucket: 'projet-api-64e59.appspot.com',
+    apiKey: 'AIzaSyCcK70zuIKLVSVWDsqeEnQJ-UCE65FSB7U',
+    authDomain: 'projet-api-64e59.firebaseapp.com',
+    messagingSenderId: '845732716561',
+  },
+	production: false
+};
