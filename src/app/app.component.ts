8b476389b8fc743f68c796bd8ff73da2638180a5
Fatou NIANG <67634804+fatou267@users.noreply.github.com>
2023-03-10 11:47:55 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: <h1>Hello word</h1>',
+  templateUrl: '<h1>Hello word</h1>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
